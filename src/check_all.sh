07a79ce012b52c192fe21e57af645d336deb68e8
张耀华 <13752354704@163.com>
2024-05-08 16:42:41 +0800

    feature: metrics hash map

--- src/check_all.sh
+++ src/check_all.sh
@@ -3,3 +3,5 @@ echo "=========  fmt code  ========="
 cargo fmt -- --check
 echo "========= clippy all-targets ========="
 cargo clippy --all-targets --all-features --tests --benches -- -D warnings
+echo "========= check all ========="
+cargo check --all
